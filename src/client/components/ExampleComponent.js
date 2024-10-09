0a01e08adfebd65596bfce497b193180b4863e96
Oleksandr Susak <susak52@gmail.com>
2024-10-09 14:23:16 +0300

    Update 1 ExampleComponent.js

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1>Welcome to My Project</h1>;
+	return (
+	    <>
+		<h1>Welcome to My Project</h1>
+		<p> Susak Oleksandr</p>
+		<p> This priject was modified </p>
+	    </>
+	);
 }
