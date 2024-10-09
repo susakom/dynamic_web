02af8f0740fe00ef68da0c38e11cbafc9c8bf4b0
Oleksandr Susak <susak52@gmail.com>
2024-10-09 15:20:54 +0300

    Update 4 ExampleComponent.js

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -4,7 +4,9 @@ export default function ExampleComponent() {
 	return (
 	    <>
 		<h1>Welcome to My Project</h1>
-		<p> Susak Oleksandr</p>
+		<p style={{ fontFamily: "Lucida Console, sans-serif", fontSize: "20px", color: "blue" }}>
+                        Susak Oleksandr
+		</p>
 		<p style={{ fontFamily: "Arial, sans-serif", fontSize: "24px", color: "black" }}> 
 			This project was modified 
 		</p>
